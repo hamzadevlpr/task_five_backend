5b4c9ff468ef35f7959ab642722fcb873193255d
Muhammad Hamza <muhammadhamzamlk@gmail.com>
2024-03-21 23:56:24 +0500

    first commit

---
+++ database/users.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2024 at 05:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userdata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `dob` varchar(30) DEFAULT NULL,
+  `hobby` varchar(30) NOT NULL,
+  `street` varchar(30) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `postCode` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.js
@@ -0,0 +1,116 @@
+
+const express = require('express');
+const app = express();
+const connector = require('./connection.js');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: true,
+        methods: ["GET", "POST"]
+    }
+
+});
+
+
+
+
+
+connector.connect((err) => {
+    if (err) {
+        console.log('Error connecting to Db');
+        return;
+    }
+    console.log('Connection established');
+}
+);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    connector.query('SELECT * FROM users', (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
+
+app.get('/:userId', (req, res) => {
+    const userId = req.params.userId;
+    connector.query('SELECT * FROM users WHERE id = ?', userId, (err, rows) => {
+        if (err) throw err;
+        res.send(rows);
+    });
+}
+);
+
+app.post('/add', (req, res) => {
+    const { name, email, phone, dob, hobby, street, country, city, state, postCode } = req.body;
+    const user = { name, email, phone, dob, hobby, street, country, city, state, postCode };
+    connector.query('INSERT INTO users SET ?', user, (err, result) => {
+        if (err) {
+            console.error('Error inserting user:', err);
+            res.status(500).send('Error adding user');
+            return;
+        }
+        res.status(200).send('User added successfully');
+    });
+});
+
+
+app.put('/update/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const { name, email, phone, dob, hobby, street, country, city, state, postCode } = req.body;
+    const updatedUser = { name, email, phone, dob, hobby, street, country, city, state, postCode };
+    connector.query('UPDATE users SET ? WHERE id = ?', [updatedUser, userId], (err, result) => {
+        if (err) {
+            console.error('Error updating user:', err);
+            res.status(500).send('Error updating user');
+            return;
+        }
+        res.status(200).send('User updated successfully');
+    });
+});
+
+app.delete('/delete/:userId', (req, res) => {
+    const userId = req.params.userId;
+    connector.query('DELETE FROM users WHERE id = ?', userId, (err, result) => {
+        if (err) {
+            console.error('Error deleting user:', err);
+            res.status(500).send('Error deleting user');
+            return;
+        }
+        res.status(200).send('User deleted successfully');
+    });
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('send', (data) => {
+        console.log(data);
+        socket.broadcast.emit('recieve', data.message);
+    });
+    socket.on('user_updated', (userId) => {
+        console.log('User updated:', userId);
+        socket.broadcast.emit('user_updated', userId);
+    });
+    socket.on('user_deleted', (userId) => {
+        console.log('User deleted:', userId);
+        socket.broadcast.emit('user_deleted', userId);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(3000, () => {
+    console.log('listening on port :3000');
+}
+);
