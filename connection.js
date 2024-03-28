00aa680106c4cd70bed0ae2ff051a4a4af2b45e4
Muhammad Hamza <muhammadhamzamlk@gmail.com>
2024-03-28 21:21:14 +0500

    make it better

--- connection.js
+++ connection.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'userdata'
+    host: 'us-cluster-east-01.k8s.cleardb.net',
+    user: 'b4fa20afc37650',
+    password: '1be673c6',
+    database: 'heroku_1379925013963b8'
 
 });
-
 
 module.exports = con;
