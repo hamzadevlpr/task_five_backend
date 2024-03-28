abf3caa8d65ebd91546818a33c4a5cd515c49d55
Muhammad Hamza <muhammadhamzamlk@gmail.com>
2024-03-29 00:04:45 +0500

    Update database connection details

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: 'us-cluster-east-01.k8s.cleardb.net',
-    user: 'b4fa20afc37650',
-    password: '1be673c6',
-    database: 'heroku_1379925013963b8'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'userdata'
 
 });
+
 
 module.exports = con;
